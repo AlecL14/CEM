ca4557def13410fde5ff1204d9e288632b60807a
JovenTung <65539698+JovenTung@users.noreply.github.com>
2024-09-21 18:34:27 +1000

    Completed Login
    included hardcoded email for admin and user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,22 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Login from './login'
-import './App.css'
-import { useEffect, useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './login';
+import UserMain from './userMain';
+import AdminMain from './adminMain'; 
+
+import './App.css';
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false)
-  const [email, setEmail] = useState('')
-
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/userHomePage" element={<UserMain />} />
+          <Route path="/adminHomePage" element={<AdminMain />} />
         </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/login.js
+++ frontend/src/login.js
@@ -1,74 +1,96 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import logo from './img/Logo.png'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import logo from "./img/Logo.png";
 
-const Login = (props) => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [emailError, setEmailError] = useState('')
-  const [passwordError, setPasswordError] = useState('')
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
 
-  const navigate = useNavigate()
+  // Use the navigate hook to programmatically navigate
+  const navigate = useNavigate();
 
   const onButtonClick = () => {
     // Set initial error values to empty
-    setEmailError('')
-    setPasswordError('')
-  
+    setEmailError("");
+    setPasswordError("");
+
     // Check if the user has entered both fields correctly
-    if ('' === email) {
-      setEmailError('Please enter your email')
-      return
+    if ("" === email) {
+      setEmailError("Please enter your email");
+      return;
     }
-  
+
     if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
-      setEmailError('Please enter a valid email')
-      return
+      setEmailError("Please enter a valid email");
+      return;
     }
-  
-    if ('' === password) {
-      setPasswordError('Please enter a password')
-      return
+
+    if ("" === password) {
+      setPasswordError("Please enter a password");
+      return;
     }
-  
+
     if (password.length < 7) {
-      setPasswordError('The password must be 8 characters or longer')
-      return
+      setPasswordError("The password must be 8 characters or longer");
+      return;
     }
- }
+
+    if (email === "admin@admin.com") {
+        navigate("/userHomePage", { state: { loggedIn: true, email } });
+    } else {
+      setEmailError("Invalid email or password");
+    }
+
+    if (email === "user@user.com") {
+        navigate("/adminHomePage", { state: { loggedIn: true, email } });
+    } else {
+      setEmailError("Invalid email or password");
+    }
+
+  };
 
   return (
-    <div className={'mainContainer'}>
-      <div className={'titleContainer'}>
-      <img src={logo}></img>
-        <div>Login</div>
-      </div>
-      <br />
-      <div className={'inputContainer'}>
-        <input
-          value={email}
-          placeholder="Enter your email here"
-          onChange={(ev) => setEmail(ev.target.value)}
-          className={'inputBox'}
-        />
-        <label className="errorLabel">{emailError}</label>
-      </div>
-      <br />
-      <div className={'inputContainer'}>
-        <input
-          value={password}
-          placeholder="Enter your password here"
-          onChange={(ev) => setPassword(ev.target.value)}
-          className={'inputBox'}
-        />
-        <label className="errorLabel">{passwordError}</label>
-      </div>
-      <br />
-      <div className={'inputContainer'}>
-        <input className={'inputButton'} type="button" onClick={onButtonClick} value={'Log in'} />
+    <div className={"mainContainer"}>
+      <div className={"mainLoginBox"}>
+        <div className={"titleContainer"}>
+          <img className={"loginLogo"} src={logo} alt="Logo"/>
+        </div>
+        <br />
+        <div className={"inputContainer"}>
+          <input
+            value={email}
+            placeholder="Enter your email here"
+            onChange={(ev) => setEmail(ev.target.value)}
+            className={"inputBox"}
+          />
+          <label className="errorLabel">{emailError}</label>
+        </div>
+        <br />
+        <div className={"inputContainer"}>
+          <input
+            value={password}
+            placeholder="Enter your password here"
+            type="password"  
+            onChange={(ev) => setPassword(ev.target.value)}
+            className={"inputBox"}
+          />
+          <label className="errorLabel">{passwordError}</label>
+        </div>
+        <br />
+        <div className={"inputContainer"}>
+          <input
+            className={"inputButton"}
+            type="button"
+            onClick={onButtonClick}
+            value={"Log in"}
+          />
+        </div>
+        <div className="loginFooter">Powered by Devblokes</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
