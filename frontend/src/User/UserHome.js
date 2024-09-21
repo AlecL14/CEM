cd60dc19c438945a1016fbbe10d17f3d8d15200f
Hina <Hinalee5250@gmail.com>
2024-09-21 20:09:15 +1000

    Event list scrollable + bugfix

--- frontend/src/User/UserHome.js
+++ frontend/src/User/UserHome.js
@@ -2,15 +2,19 @@ import React, { useState } from 'react'
 import UserNavbar from "./UserNavbar";
 import UserHomeFilterBar from "./UserHomeFilterBar";
 import UserHomeEventList from "./UserHomeEventList"
+import UserHomeCalendar from "./UserHomeCalendar";
 
 const UserHome = () => {
 
     return(
-        <div className = "UserHomeContainer">
-            <UserNavbar />
-            <UserHomeFilterBar />
-            <UserHomeEventList />
-            
+        <div className="UserHomeContainer">
+            <UserNavbar/>
+            <UserHomeFilterBar/>
+            <div className="UserHomeContent">
+                <div className="UserHomeLeft"></div>
+                <div className="UserHomeMiddle"><UserHomeEventList/></div>
+                <div className="UserHomeRight"><UserHomeCalendar /></div>
+            </div>
         </div>
     )
 }
