6c6ccce4525a9443eb48fa516eec0a4e8879501d
Hina <Hinalee5250@gmail.com>
2024-09-21 23:18:51 +1000

    Fixed events

--- frontend/src/User/UserHomeEventList.js
+++ frontend/src/User/UserHomeEventList.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import "./UserHomeEventList.css"
-import eventImage from "../img/Hackathon.png"
+import HackathonImage from "../img/Hackathon.png"
+import NatureImage from "../img/NatureWalk.png"
+import PettingZooImage from "../img/PettingZoo.png"
+import PlushyImage from "../img/Plushy.png"
 
 import UserHomeEvent from "./UserHomeEvent"
 
@@ -10,17 +13,35 @@ const UserHomeEventList = () => {
         <div className="userHomeEventListContainer">
             <UserHomeEvent
                 title="SWITCH x CSIT Hackathon | @Switch & @CSIT"
-                image={eventImage}
+                image={HackathonImage}
                 date="14-16 Sept 2024"
                 fee="Free"
                 location="TBD"
                 description="Come join us for an amazing week of coding!"
             />
             <UserHomeEvent
+                title="Nature walk with Brunch | @Walking club"
+                image={NatureImage}
+                date="19 Oct 2024"
+                fee="19"
+                location="Carlton Gardens"
+                description="Take a walk with us through the beautiful carlton gardens, followed by a brunch and chat at the amazing Grand Tea Cafe!"
             />
             <UserHomeEvent
+                title="Sow a plushy | @Sowing club"
+                image={PlushyImage}
+                date="10 Nov 2024"
+                fee="Free"
+                location="80:03:009"
+                description="Who doesnt love plushies? Come make a plushy together! Yarn is provided, but feel free to bring your favourite colors!"
             />
             <UserHomeEvent
+                title="Petting Zoo @ Student Central | @Rusu"
+                image={PettingZooImage}
+                date="19 Nov 2024"
+                fee="5"
+                location="Student Central"
+                description="Were going to have all sorts of baby animals at student central on the 19th of Nov. Don’t miss this chance to cuddle with the cutest nature has to offer!"
             />
         </div>
     )
